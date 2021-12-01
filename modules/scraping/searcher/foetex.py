d7c1ff78df1db078c8044d84e3fce529e3b0cacb
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-12-01 22:08:22 +0000

    feat: can find all products from anything on foetex

--- modules/scraping/searcher/foetex.py
+++ modules/scraping/searcher/foetex.py
@@ -1,10 +1,18 @@
 from time import sleep
+from typing import List
+
 from browser import get_browser
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from lxml import etree
+
+from exam_project.modules.scraping.cereal import Cereal, Nutrition
+
+ROOT_URL = "https://hjem.foetex.dk"
 
 
-def get_foetex_page(search_name: str):
-    browser = get_browser("https://hjem.foetex.dk/")
+def get_foetex_page(search_name: str) -> List[Cereal]:
+    browser = get_browser(ROOT_URL)
 
     # click on cookie accept
     elem = browser.find_element_by_xpath('//*[@id="coiPage-1"]/div[2]/div[1]/button[3]')
@@ -21,14 +29,119 @@ def get_foetex_page(search_name: str):
 
     sleep(3)
 
-    return browser.page_source
+    links = __get_links(browser.page_source)
+
+    browser.close()
+
+    with ThreadPoolExecutor(len(links)) as ex:
+        return list(filter(lambda c: c != None, ex.map(__get_single_cereal, links)))
+
+
+def __get_links(page_source) -> List[str]:
+    soup = BeautifulSoup(page_source, "html.parser")
+    tags = soup.find_all("a", href=True)
+    links: List[str] = []
+    for a in tags:
+        url = a["href"]
+        if "/produkt" in url:
+            links.append(ROOT_URL + url)
+    return links
+
+
+def __get_single_cereal(link: str) -> Cereal:
+    browser = __get_correct_browser_state(link)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    dom = etree.HTML(str(soup))
+    browser.close()
+
+    name = dom.xpath(
+        '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/article/div[3]/span'
+    )[0].text
+    brand = dom.xpath(
+        '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/article/div[4]/span/strong'
+    )[0].text
+    price = __make_float(
+        dom.xpath(
+            '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/div[1]/div[1]/div[2]/div/span/text()[4]'
+        )[0]
+    )
+    grams = __make_float(
+        dom.xpath(
+            '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/article/div[4]/span/text()[2]'
+        )[0]
+    )
+
+    tbody = dom.xpath(
+        '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/div[2]/section[2]/div/div/div/table/tbody'
+    )
+    if len(tbody) == 0:
+        # Cannot fine Cornflakes Salling tbody for some reason
+        return None
+
+    html = etree.HTML(etree.tostring(tbody[0], pretty_print=True))
+    fat = __get_nutrition(html, "Fedt")
+    protein = __get_nutrition(html, "Protein")
+    carbohydrates = __get_nutrition(html, "Kulhydrater")
+    fiber = __get_nutrition(html, "Kostfibre")
+    salt = __get_nutrition(html, "Salt")
+
+    return Cereal(
+        name,
+        brand,
+        price,
+        grams,
+        Nutrition(protein, carbohydrates, fiber, fat, salt),
+    )
+
+
+def __get_correct_browser_state(link: str):
+    browser = get_browser(link)
+    browser.find_element_by_xpath(
+        '//*[@id="coiPage-1"]/div[2]/div[1]/button[3]'
+    ).click()
+    sleep(2)
+    browser.find_element_by_xpath(
+        '//*[@id="__next"]/div[1]/main/div[1]/div[1]/section[2]/div[2]/section[2]/button'
+    ).click()
+
+    sleep(3)
+    return browser
+
+
+def __get_nutrition(tbody_html, name: str):
+    items = []
+    stop = False
+    for item in tbody_html.iter("span"):
+        text = item.text
+        items.append(text)
+        if stop:
+            break
+
+        if text == name:
+            stop = True
+
+    return __make_float(__remove_chars(items[-1]))
+
+
+def __make_float(danish_number: str) -> float:
+    return float(danish_number.replace(",", "."))
+
+
+def __remove_chars(number_with_string: str) -> str:
+    return number_with_string.replace(" g", "")
 
 
 if __name__ == "__main__":
-    name = "Cornflakes".lower()
-    soup = BeautifulSoup(get_foetex_page(name), "html.parser")
-    tags = soup.find_all("a", href=True)
-    for a in tags:
-        url = a["href"]
-        if name and "/produkt" in url:
-            print("url: ", url)
+    cereals = get_foetex_page("muesli")
+    for cereal in cereals:
+        print(
+            cereal.name,
+            cereal.brand,
+            cereal.grams,
+            cereal.price,
+            cereal.nutrition.fat,
+            cereal.nutrition.protein,
+            cereal.nutrition.carbohydrates,
+            cereal.nutrition.fiber,
+            cereal.nutrition.salt,
+        )
